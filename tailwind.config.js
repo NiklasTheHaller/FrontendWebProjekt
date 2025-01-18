a4de6ad17870735a3004773a3cb8d9ac2edde175
NiklasTheHaller <niklash2021@gmail.com>
2025-01-18 15:01:45 +0100

    pushing small changes I forgot to push months ago

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./public/index.html", "src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#9381FF",
+      background: "#272727",
+      accent: "#5C946E",
+      light: "#E0FBFC",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
